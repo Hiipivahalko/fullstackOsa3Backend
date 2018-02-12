88fd01d2bc39ec6b2cd32dc8811272219d50a777
Ossi <ossi.siipola@helsinki.fi>
2018-02-12 12:41:12 +0200

    lets bring front

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ const cors = require('cors')
 app.use(bodyParser.json())
 app.use(morgan('tiny'))
 app.use(cors())
+app.use(express.static('build'))
 
 morgan.token('data', function(req, res) { 
     return JSON.stringify(req.body)
